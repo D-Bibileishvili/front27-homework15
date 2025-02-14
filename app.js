3b29147d2a93ed04a8bdeb792e4296baf87bfa46
D-Bibileishvili <d.bibileishvili19@gmail.com>
2025-02-14 22:20:20 +0400

    add code in app.js and test in console.log

---
+++ app.js
@@ -0,0 +1,19 @@
+const userName = "Smith";
+const userHeight = 1.78;
+const isMarried = true;
+let userJob = undefined;
+let userNumber = null;
+
+userJob = "teacher";
+
+console.log(
+  userName + " is " + userHeight + " heights" + " and he is " + userJob
+);
+
+const name = "davit";
+let dayOfWeek = "Friday";
+const favoriteActivity = "coding";
+
+console.log(
+  `Hi, my name is ${name}! Today is ${dayOfWeek}, a perfect day for ${favoriteActivity}.`
+);
